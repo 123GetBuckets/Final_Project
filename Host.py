79ac6a83c9bb997033085b0769cff22dd0559f53
Nkorsah <Nkorsah21@gmail.com>
2022-11-27 22:25:54 -0500

    Update Host.py

--- Host.py
+++ Host.py
@@ -1,6 +1,8 @@
-import socket 
+import socket
 import threading
 import time
+
 address = ('10.109.88.158', 5040)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(address)
+# This is my branch

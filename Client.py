f0013fc56d4c70654f90ad612ceaf669b1b81818
HMISTER <kamanahemanth123@gmail.com>
2022-11-29 18:15:01 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,7 +1,7 @@
 import socket
 import getpass
 user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-addr = ('10.109.95.251', 444)
+addr = ('10.109.89.148', 444)
 user.connect(addr)
 
 while True:

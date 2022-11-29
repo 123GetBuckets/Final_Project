3643cc1423891cf8950a9adb3c69f51b654bfe95
HMISTER <kamanahemanth123@gmail.com>
2022-11-29 17:04:20 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -2,8 +2,6 @@ import socket
 user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ('10.109.95.251', 444)
 user.connect(addr)
-addr = ('10.109.83.27', 444)
-user.connect(addr)
 
 while True:
     msg = input()

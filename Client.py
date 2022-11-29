a0874e93e6371b45f2e4049204e2147cb44ca240
HMISTER <kamanahemanth123@gmail.com>
2022-11-29 18:23:02 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,11 +1,11 @@
 import socket
-import getpass
 user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ('10.109.89.148', 444)
 user.connect(addr)
 
 while True:
-    msg = getpass.getpass()
+    msg = input()
+    print("\x1B[F\x1B[2K", end="")
     if msg.lower() == "terminate":
         break
     user.send(bytes(msg, encoding='ascii'))

c4824a438b750ae3497e561d713b45f0264a27e7
Nkorsah <Nkorsah21@gmail.com>
2022-11-29 15:45:35 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,9 +1,7 @@
 import socket
 user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-<<<<<<< HEAD
 addr = ('10.109.95.251', 444)
 user.connect(addr)
-=======
 addr = ('10.109.83.27', 444)
 user.connect(addr)
 
@@ -12,6 +10,4 @@ while True:
     if msg.lower() == "terminate":
         break
     user.send(bytes(msg, encoding='ascii'))
-
-    
->>>>>>> 79e0cb80742d5be53ad8099342fe917e7a7e55b6
+# hi

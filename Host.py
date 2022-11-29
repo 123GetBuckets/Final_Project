a95ca0afa0677bccc2dc87768bf9d8958f579905
HMISTER <kamanahemanth123@gmail.com>
2022-11-28 22:45:02 -0500

    Update Host.py

--- Host.py
+++ Host.py
@@ -8,6 +8,9 @@ class host:
         self.HOST = HOST
         self.PORT = PORT
 
+    def c_handle(self, conn, c_addr):
+        print(f'[CONNECTED]:{c_addr}')
+
     def start(self):
         print(f'[SERVER START]')
         time.sleep(1)
@@ -17,22 +20,20 @@ class host:
         self.name = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         addr = (self.HOST, self.PORT)
         self.name.bind(addr)
-        
         self.name.listen()
         while True:
             conn, c_addr = self.name.accept()
-            thread = threading.Thread(target = c_handle, args = (conn, c_addr))
+            thread = threading.Thread(target=self.c_handle, args = (conn, c_addr))
             thread.start()
 
-    def c_handle(self, conn, c_addr):
-        print(f'[CONNECTED]:{c_addr}')
+    
 
 
 
 
 
         
-server = host("sock", '10.109.88.158', 444)
+server = host("sock", '10.109.95.251', 444)
 server.start()
 
 

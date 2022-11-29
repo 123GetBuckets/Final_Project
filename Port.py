b30aad38d7abfa92fc76f05aedc5b28d40518ab0
Nkorsah <Nkorsah21@gmail.com>
2022-11-29 17:06:35 -0500

    Create Port.py

---
+++ Port.py
@@ -0,0 +1,20 @@
+import socket
+import time
+import subprocess
+import sys
+
+breacher = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+try:
+    for port in range(1, 300):
+        # print(port)
+        addr = ('207.244.91.154', port)
+        x = breacher.connect_ex(addr)
+        if x == 0:
+            print(port, " is open")
+        breacher.close()
+except socket.error:
+    sys.exit()
+except socket.gaierror:
+    sys.exit()

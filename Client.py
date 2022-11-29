3cc05f72e111b3b631339dece5a4f64318585211
Nkorsah <Nkorsah21@gmail.com>
2022-11-29 18:36:36 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,12 +1,14 @@
 import socket
+import threading
+
 user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ('10.109.89.148', 444)
 user.connect(addr)
 
 while True:
     msg = input()
-    print("\x1B[F\x1B[2K", end="")
+    print(str(msg1, encoding="utf-8"))
     if msg.lower() == "terminate":
         break
     user.send(bytes(msg, encoding='ascii'))
-
+    msg1 = user.recv(1024)

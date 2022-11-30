7c366e1568fc56e44937877f8d4d04e0a880eb2c
HMISTER <kamanahemanth123@gmail.com>
2022-11-30 13:49:08 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,11 +1,6 @@
 import socket
 import threading
-
 user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-addr = ('10.109.89.148', 444)
-user.connect(addr)
-
-
 def sending():
     while True:
         msg = input()
@@ -13,18 +8,19 @@ def sending():
             break
         user.send(bytes(msg, encoding='ascii'))
 
-
+    user.close()
 def rec():
     while True:
         msg = user.recv(1024)
         print(f'{str(msg, encoding="utf-8")}')
 
 
-def start():
+def start(IP, PORT):
+    addr = (IP, PORT)
+    user.connect(addr)
     thread = threading.Thread(target=sending)
     thread2 = threading.Thread(target=rec)
     thread.start()
     thread2.start()
 
-
-start()
+start('10.109.81.174', 444)

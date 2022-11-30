e5631720df533a1f7dd9b5a58ba96a0f2ca0192a
Nkorsah <Nkorsah21@gmail.com>
2022-11-29 19:50:59 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -5,10 +5,26 @@ user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ('10.109.89.148', 444)
 user.connect(addr)
 
-while True:
-    msg = input()
-    print(str(msg1, encoding="utf-8"))
-    if msg.lower() == "terminate":
-        break
-    user.send(bytes(msg, encoding='ascii'))
-    msg1 = user.recv(1024)
+
+def sending():
+    while True:
+        msg = input()
+        if msg.lower() == "terminate":
+            break
+        user.send(bytes(msg, encoding='ascii'))
+
+
+def rec():
+    while True:
+        msg = user.recv(1024)
+        print(str(msg, encoding="utf-8"))
+
+
+def start():
+    thread = threading.Thread(target=sending)
+    thread2 = threading.Thread(target=rec)
+    thread.start()
+    thread2.start()
+
+
+start()

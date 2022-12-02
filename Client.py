de8e1d841d610d0beee310c8cdce57aad3d37b8c
HMISTER <kamanahemanth123@gmail.com>
2022-12-01 19:33:35 -0500

    Update Client.py

--- Client.py
+++ Client.py
@@ -3,24 +3,39 @@ import threading
 user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def sending():
     while True:
-        msg = input()
-        if msg.lower() == "terminate":
-            break
-        user.send(bytes(msg, encoding='ascii'))
-
-    user.close()
+        try:
+            msg = input()
+            if msg.lower() == "terminate":
+                user.close()
+                return False
+            if not msg:
+                return False
+            user.send(bytes(msg, encoding='ascii'))
+        except:
+            print("SEND ERROR")
+            user.close()
+            return False
+    
 def rec():
     while True:
-        msg = user.recv(1024)
-        print(f'{str(msg, encoding="utf-8")}')
-
+        try:    
+            msg = user.recv(1024)
+            print(f'{str(msg, encoding="utf-8")}')
+        except:
+            print("RECIEVE ERROR")
+            user.close()
+            return False
 
 def start(IP, PORT):
     addr = (IP, PORT)
-    user.connect(addr)
-    thread = threading.Thread(target=sending)
-    thread2 = threading.Thread(target=rec)
+    try:
+        user.connect(addr)
+    except:
+        print("Unable to reach HOST")
+        return 
+    thread = threading.Thread(target=rec)
+    thread2 = threading.Thread(target=sending)
     thread.start()
     thread2.start()
-
-start('10.109.81.174', 444)
+    return
+start('10.109.82.246', 444)

c3ee9d0645e4c3d6c2fe732ace3818bc86297367
jacobrufel-coder <tun40160@temple.edu>
2022-12-04 12:43:34 -0500

    Update INTERFACE.py
    yooo

--- INTERFACE.py
+++ INTERFACE.py
@@ -15,39 +15,54 @@ b_blue = "#89CFF0"
 blue = "#00FFFF"
 text_color = "#EAECEE"
 font = "Helvetica"
+root.resizable(width=FALSE, height=FALSE)
 
 
+def creation():
+
+    top_page = Label(root, bg=b_blue, fg="black", text="Welcome to Chat Bucket!", pady=1, padx=1, height=1).grid(row=0) # my top page
+
+send = Button(root, text="Send", bg=b_blue, command=lambda:onclick(1)) # send button
+send.grid(row=2, column=1)
+
+rectangle = Text(root, width=60, height=50, bg=b_blue) # rectangle 
+rectangle.config(state=DISABLED)
+rectangle.grid(row=1, column=0, columnspan=2) # grid
 
 
-def send():
+chat_box = Entry(root, bg=b_blue, fg="white",width=55) # chat box
+chat_box.grid(row=2, column=0)
 
-    enter = Entry(root, bg="#2C3E50", fg=text_color, font=font, width=55)
-    enter.grid(row=2, column=0)
-    send_button = "You:  " + enter.get()
-    
+
+scrollbar = Scrollbar(rectangle)
+scrollbar.place(relheight=1, relx=0.974)
+
+def onclick(args): 
+    text = chat_box.get()
+    if args  == 1:
+        rectangle.config(state=NORMAL)
+        rectangle.insert(END, "\n" + "YOU:" + str(text))
+        rectangle.config(state=DISABLED)
+    chat_box.delete()
 
 
 
 def main():
+    creation()
+    onclick(args=1)
 
-    top_page = Label(root, bg=b_blue, fg="black", text="Welcome to Chat Bucket!", pady=10, width=20, height=1).grid(row=0) # my top page
+main()
 
 
-    send = Button(root, text="Send", bg=b_blue, command=main).grid(row=2, column=1) # send button
 
-    rectangle = Frame(root, width=600, height=500, bg=b_blue) # rectangle 
 
-    rectangle.grid(row=1, column=0, columnspan=2) # grid
+root.mainloop()
 
     
-    chat_box = Entry(root, bg=b_blue, fg="white",width=55) # chat box
-    chat_box.grid(row=2, column=0)
+# what to do left: Make "you" on right and "Connector" on left
+# Make it type out on each side, 
+# make 
 
-
-    scrollbar = Scrollbar(rectangle)
-    scrollbar.place(relheight=1, relx=0.974)
-
-    root.mainloop()
 '''
     logo_image = ImageTk.PhotoImage(file="bucket.png")
     widget_image = Label(rectangle,image=logo_image)
@@ -55,9 +70,7 @@ def main():
     widget_image.pack()
 '''
 
+
+
+
     
-
-
-
-
-main()

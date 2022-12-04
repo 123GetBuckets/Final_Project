faf8d47c0a43161d94af250b79ffd6e988be5428
Nkorsah <Nkorsah21@gmail.com>
2022-12-04 14:18:07 -0500

    Update INTERFACE.py

--- INTERFACE.py
+++ INTERFACE.py
@@ -1,12 +1,9 @@
-from tkinter import *
-import os
-import threading
+import tkinter as tk
 
-from PIL import ImageTk
+# from PIL import ImageTk
 
 
-
-root = Tk()
+root = tk.Tk()
 root.title("CHAT BUCKET")
 root.eval('tk::PlaceWindow . center')
 
@@ -15,53 +12,59 @@ b_blue = "#89CFF0"
 blue = "#00FFFF"
 text_color = "#EAECEE"
 font = "Helvetica"
-root.resizable(width=FALSE, height=FALSE)
+
+root.resizable(width=tk.FALSE, height=tk.FALSE)
 
 
 def creation():
 
-    top_page = Label(root, bg=b_blue, fg="black", text="Welcome to Chat Bucket!", pady=1, padx=1, height=1).grid(row=0) # my top page
+    top_page = tk.Label(root, bg=b_blue, fg="black",
+                        text="Welcome to Chat Bucket!",
+                        pady=1, padx=1, height=1).grid(row=0)  # my top page
 
-send = Button(root, text="Send", bg=b_blue, command=lambda:onclick(1)) # send button
+
+send = tk.Button(root, text="Send", bg=b_blue, command=lambda: onclick(1))
 send.grid(row=2, column=1)
 
-rectangle = Text(root, width=60, height=50, bg=b_blue) # rectangle 
-rectangle.config(state=DISABLED)
-rectangle.grid(row=1, column=0, columnspan=2) # grid
+root.bind('<Return>', lambda event: onclick(1))
+
+rectangle = tk.Text(root, width=60, height=50, bg=b_blue)  # rectangle
+rectangle.config(state=tk.DISABLED)
+rectangle.grid(row=1, column=0, columnspan=2)  # grid
 
 
-chat_box = Entry(root, bg=b_blue, fg="white",width=55) # chat box
+chat_box = tk.Entry(root, bg=b_blue, fg="white", width=55)  # chat box
 chat_box.grid(row=2, column=0)
 
 
-scrollbar = Scrollbar(rectangle)
+scrollbar = tk.Scrollbar(rectangle)
 scrollbar.place(relheight=1, relx=0.974)
 
-def onclick(args): 
-    text = chat_box.get()
-    if args  == 1:
-        rectangle.config(state=NORMAL)
-        rectangle.insert(END, "\n" + "YOU:" + str(text))
-        rectangle.config(state=DISABLED)
-    chat_box.delete()
 
+def onclick(args):
+    text = chat_box.get()
+    if len(text) != 0:
+        if args == 1:
+            rectangle.config(state=tk.NORMAL)
+            rectangle.insert(tk.END, "\n" + "YOU:" + str(text))
+            rectangle.config(state=tk.DISABLED)
+        chat_box.delete(0, tk.END)
 
 
 def main():
     creation()
     onclick(args=1)
 
+
 main()
-
-
 
 
 root.mainloop()
 
-    
+
 # what to do left: Make "you" on right and "Connector" on left
-# Make it type out on each side, 
-# make 
+# Make it type out on each side,
+# make
 
 '''
     logo_image = ImageTk.PhotoImage(file="bucket.png")
@@ -69,8 +72,3 @@ root.mainloop()
     widget_image.image = logo_image
     widget_image.pack()
 '''
-
-
-
-
-    

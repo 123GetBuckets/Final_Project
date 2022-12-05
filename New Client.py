f18fe376679a5d2e7f4139813ab21f8408bc2838
Nkorsah <Nkorsah21@gmail.com>
2022-12-04 21:34:27 -0500

    jfkd;jsfskd;jsdfk;daj

--- New Client.py
+++ New Client.py
@@ -66,11 +66,12 @@ class client:
         self.blue = "#00FFFF"
         self.text_color = "#EAECEE"
         self.font = "Helvetica"
-        self.send = tk.Button(self.root, text="Send", bg=self.b_blue, command=lambda: self.send())
+        self.send = tk.Button(self.root, text="Send", bg=self.b_blue, command=lambda: self.sender())
         self.rectangle = tk.Text(self.root, width=60, height=50, bg=self.b_blue)  # rectangle
         self.chat_box = tk.Entry(self.root, bg=self.b_blue, fg="black", width=55)
         self.scrollbar = tk.Scrollbar(self.rectangle)
         self.Switch = False
+        self.rmsg = ''
 
     def creation(self):
         self.top_page = tk.Label(self.root, bg=self.b_blue, fg="black",
@@ -79,13 +80,13 @@ class client:
         self.root.eval('tk::PlaceWindow . center')
         self.root.resizable(width=tk.FALSE, height=tk.FALSE)
         self.send.grid(row=2, column=1)
-        self.root.bind('<Return>', lambda event: self.onclick(1))
+        self.root.bind('<Return>', lambda event: self.sender())
         self.rectangle.config(state=tk.DISABLED)
         self.rectangle.grid(row=1, column=0, columnspan=2)  # grid
         self.chat_box.grid(row=2, column=0)
         self.scrollbar.place(relheight=1, relx=0.974)
 
-    def send(self):
+    def sender(self):
         self.Switch = True
 
     def onclick(self, args):  # send button
@@ -106,24 +107,24 @@ class client:
     def Nsending(self):
         while True:
             if self.Switch is True:
-                if self.onclick(1) != False:
-                    try:
+                try:
+                    if self.onclick(1) != False:
                         self.user.send(bytes(self.msg, encoding='ascii'))
                         self.Switch = False
-                    except:
-                        print("SEND ERROR")
-                        self.user.close()
-                        return False
+                        print(self.Switch)
+                except:
+                    print("SEND ERROR")
+                    self.user.close()
+                    return False
 
     def Nrec(self):
         while True:
             try:
-                rmsg = self.user.recv(1024)
-                if len(rmsg) == 0:
-                    return
+                self.rmsg = self.user.recv(1024)
                 self.rectangle.config(state=tk.NORMAL)
-                self.rectangle.insert(tk.end, '\n' + str(rmsg, encoding='utf-8'))
+                self.rectangle.insert(tk.END, '\n' + str(self.rmsg, encoding='utf-8'))
                 self.rectangle.config(state=tk.DISABLED)
+
             except:
                 print("RECIEVE ERROR")
                 self.user.close()
@@ -131,7 +132,7 @@ class client:
 
     def start(self, IP, PORT):
         self.creation()
-        time.sleep(10)
+        time.sleep(5)
         addr = (IP, PORT)
         try:
             self.user.connect(addr)
@@ -141,9 +142,9 @@ class client:
         # print('connect')
         self.thread = threading.Thread(target=self.Nrec)
         self.thread2 = threading.Thread(target=self.Nsending)
-        self.creation()
         self.thread.start()
         self.thread2.start()
+        self.creation()
 
 
 gui = client()
